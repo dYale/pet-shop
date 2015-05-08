54b74f85ba025c9151c0f6e5b2b8b7492d2d1baa
MKS-Student <mks.student@hackreactor.com>
2015-05-08 12:59:50 -0700

    first API access

--- client/components/pet-shop/pet-shop.js
+++ client/components/pet-shop/pet-shop.js
@@ -5,11 +5,16 @@
   PetShop.controller = function () {
     var ctrl = this
     ctrl.shop = m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1' })
+    ctrl.pets = m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1/pets'})
   }
 
   PetShop.view = function (ctrl) {
     return m('.pet-shop', [
-      m('h1', "Welcome to " + ctrl.shop().name)
+      m('h1', "Welcome to " + ctrl.shop().name),
+      console.log(ctrl.pets().name),
+      m('h2', 'our pets are:' + ctrl.pets().map(function(animal){
+        return animal.species})
+      )
     ])
   }
 
