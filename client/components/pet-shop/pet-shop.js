07fd51f819c6c9dba34dd9edc38d5569dda383a4
MKS-Student <mks.student@hackreactor.com>
2015-05-08 13:56:34 -0700

    basic styling and inventory

--- client/components/pet-shop/pet-shop.js
+++ client/components/pet-shop/pet-shop.js
@@ -6,15 +6,28 @@
     var ctrl = this
     ctrl.shop = m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1' })
     ctrl.pets = m.request({ method: 'GET', url: 'http://pet-shop.api.mks.io/shops/1/pets'})
+    ctrl.myclickhandler = function(){return 'clicked!'};
+
   }
 
   PetShop.view = function (ctrl) {
     return m('.pet-shop', [
       m('h1', "Welcome to " + ctrl.shop().name),
-      console.log(ctrl.pets().name),
-      m('h2', 'our pets are:' + ctrl.pets().map(function(animal){
-        return animal.species})
-      )
+      m('h2', "Check out our furry inventory:"),
+      ctrl.pets().map(function(animal){
+        return m('.container', [
+          m("img", { src: animal.imageUrl, height: '200px', width: '200px'}),
+          m('br'),
+          m('name', animal.name + " the " + (animal.species[0].toUpperCase()) + animal.species.slice(1)),
+          m('br'),
+          console.log(),
+          m("button", {onclick: ctrl.myclickhandler}, animal.likes)])
+
+
+
+
+        })
+      
     ])
   }
 
